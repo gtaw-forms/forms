e3b1f9f5ee153c90bf0897e391b5141e8eb2a645
LPX-Dev <73806627+LPX-Dev@users.noreply.github.com>
2026-02-01 13:23:36 +0000

    chore: ignore functions/ directory to protect backend data

--- functions/src/utils/firebase.js
+++
@@ -1,9 +0,0 @@
-import admin from "firebase-admin";
-
-// Initialize Firebase Admin SDK
-if (admin.apps.length === 0) {
-    admin.initializeApp();
-}
-
-export const db = admin.database();
-export { admin };
